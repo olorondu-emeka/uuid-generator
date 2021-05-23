29a905e50dedfe9a93351287b0fafbd6596c9ea6
olorondu-emeka <oloronduchukwuemeka@gmail.com>
2021-05-23 19:01:05 +0100

    partially completed GET function

--- index.py
+++ index.py
@@ -1,11 +1,29 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from datetime import datetime
+import json
 import redis
+import uuid
 
 # connect to redis
-client = redis.Redis(host='localhost', port=6379)
+redisClient = redis.Redis(host='localhost', port=6379)
 
-# set a key
-client.set('test-key', 'test-value')
 
-# get a value
-value = client.get('test-key')
-print(value)
+class ServiceHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        currentTimestamp = str(datetime.now())
+        simpleUUID = uuid.uuid1()
+
+        redisClient.hset('uuid', currentTimestamp, str(simpleUUID.hex))
+        finalData = redisClient.hgetall('uuid')
+
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(finalData))
+
+        # self.wfile.write(json.dumps(finalData).encode())
+
+
+# Server Initialization
+server = HTTPServer(('localhost', 8080), ServiceHandler)
+server.serve_forever()
